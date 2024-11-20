805b195dfcb9a2abae70eec4addd555a18473b2f
Pedro Henrique Passos Carreira <peagacarreira@gmail.com>
2024-11-20 15:29:46 -0300

    alterando caminhos

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
 
     environment {
         UNITY_PATH = 'C:\\Program Files\\Unity\\Hub\\Editor\\6000.0.24f1\\Editor\\Unity.exe'
-        PROJECT_PATH = 'C:\\Jenkins\\workspace\\Unity\\AssassinsChrisGame'
+        PROJECT_PATH = 'C:\\jenkins\\workspace\\AssassinsChris'
         //EMAIL_RECIPIENTS = credentials('email-recipients')
     }
 
