debce6f95822326849ec49439877269173029888
Pedro Henrique Passos Carreira <peagacarreira@gmail.com>
2024-11-20 15:50:02 -0300

    artefato

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -32,8 +32,8 @@ pipeline {
 
     post {
         always {
-            archiveArtifacts artifacts: 'AssassinsChris//Builds/**.zip', allowEmptyArchive: true
-            archiveArtifacts artifacts: 'AssassinsChris//Builds/TestResults/*.xml', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'AssassinsChris/Builds/**.zip', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'AssassinsChris/Builds/TestResults/*.xml', allowEmptyArchive: true
         }
         
     }
