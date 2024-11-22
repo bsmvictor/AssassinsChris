bc93784369b655a4155be0dfa30230dfe91e8487
Victor Boaventura <victor.boaventura@gec.inatel.br>
2024-11-22 00:37:02 -0300

    Update jenkinsfile to build using docker

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,57 +1,66 @@
 pipeline {
-    agent { label 'windows-node' }
+    agent {
+        docker {
+            image 'unityci/editor:6000.0.28f1-webgl-3'
+            args '-v /c/Jenkins/workspace/Unity/AssassinsChrisGame:/workspace'
+        }
+    }
 
     environment {
-        UNITY_PATH = 'C:\\Program Files\\Unity\\Hub\\Editor\\6000.0.24f1\\Editor\\Unity.exe'
-        PROJECT_PATH = 'C:\\jenkins\\workspace\\AssassinsChris'
-    //EMAIL_RECIPIENTS = credentials('email-recipients')
-    }
-
-    parameters {
-        booleanParam(name: 'Windows_build', defaultValue: true, description: 'Executar Build para Windows')
-        booleanParam(name: 'WebGL_build', defaultValue: true, description: 'Executar Build para WebGL')
-    }
-
-    triggers {
-        cron('0 8-16 * * 1-5')
+        UNITY_LICENSE = credentials('unity-license-key') // Adicione sua licença Unity às credenciais do Jenkins
+        PROJECT_PATH = 'C:\\jenkins_home\\workspace\\Unity\\AssassinsChrisGame' // Caminho do projeto Unity dentro do container
     }
 
     stages {
-        stage('Run Tests') {
+        stage('Activate License') {
             steps {
-                bat """
-                "${env.UNITY_PATH}" -quit -batchmode -projectPath "${env.PROJECT_PATH}" -executeMethod TestReportGenerator.RunTestsAndGenerateReport -logFile -
+                echo 'Ativando licença do Unity...'
+                sh """
+                unity-editor \
+                -batchmode \
+                -logFile /dev/stdout \
+                -username <SEU_USUÁRIO_UNITY> \
+                -password <SUA_SENHA_UNITY> \
+                -serial ${UNITY_LICENSE}
                 """
             }
         }
 
-         stage('Build Windows') {
-            when({expression {params.Windows_build}})
-             steps {
-                 echo "Build Windows executada em: ${new Date()}"
-                 bat """
-                 "${env.UNITY_PATH}" -quit -batchmode -projectPath "${env.PROJECT_PATH}" -executeMethod BuildScript.BuildWindows -logFile -
-                 """
-             }
+        stage('Run Tests') {
+            steps {
+                echo 'Executando testes...'
+                sh """
+                unity-editor \
+                -quit -batchmode \
+                -projectPath ${PROJECT_PATH} \
+                -executeMethod TestReportGenerator.RunTestsAndGenerateReport \
+                -logFile /dev/stdout
+                """
+            }
         }
 
         stage('Build WebGL') {
-            when({expression {params.WebGL_build}})
             steps {
-                echo "Build WebGL executada em: ${new Date()}"
-                bat """
-                "${env.UNITY_PATH}" -quit -batchmode -projectPath "${env.PROJECT_PATH}" -executeMethod BuildScript.BuildWebGL -logFile -
+                echo 'Iniciando build para WebGL...'
+                sh """
+                unity-editor \
+                -quit -batchmode \
+                -projectPath ${PROJECT_PATH} \
+                -executeMethod BuildScript.BuildWebGL \
+                -buildTarget WebGL \
+                -logFile /dev/stdout
                 """
             }
         }
     }
-    
+
     post {
-        always {
-            archiveArtifacts artifacts: 'Builds/**.zip', allowEmptyArchive: true
-            archiveArtifacts artifacts: 'Builds/TestResults/*.xml', allowEmptyArchive: true
+        success {
+            echo 'Build concluída com sucesso!'
+            archiveArtifacts artifacts: 'Builds/**', allowEmptyArchive: true
+        }
+        failure {
+            echo 'Build falhou. Verifique os logs.'
         }
     }
 }
-
-
