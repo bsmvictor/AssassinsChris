702e3dcc317fb601091bc45ffbc682191a30eb3d
Pedro Henrique Passos Carreira <peagacarreira@gmail.com>
2024-11-26 00:18:50 -0300

    restaurando mais ou menos

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,13 +3,20 @@ pipeline {
 
     environment {
         UNITY_PATH = 'C:\\Program Files\\Unity\\Hub\\Editor\\6000.0.24f1\\Editor\\Unity.exe'
-        PROJECT_PATH = 'C:\\jenkins\\workspace'
+        PROJECT_PATH = 'C:\\jenkins\\workspace\\AssassinsChris'
     //EMAIL_RECIPIENTS = credentials('email-recipients')
     }
 
+    parameters {
+        booleanParam(name: 'Windows_build', defaultValue: true, description: 'Executar Build para Windows')
+        booleanParam(name: 'WebGL_build', defaultValue: true, description: 'Executar Build para WebGL')
+    }
+
+    triggers {
+        cron('0 8-16 * * 1-5')
+    }
 
     stages {
-        
         stage('Run Tests') {
             steps {
                 bat """
@@ -19,28 +26,30 @@ pipeline {
         }
 
          stage('Build Windows') {
-            steps {
+            when({expression {params.Windows_build}})
+             steps {
                  echo "Build Windows executada em: ${new Date()}"
                  bat """
                  "${env.UNITY_PATH}" -quit -batchmode -projectPath "${env.PROJECT_PATH}" -executeMethod BuildScript.BuildWindows -logFile -
                  """
-            }
+             }
         }
 
-       /* stage('Build WebGL') {
+        stage('Build WebGL') {
+            when({expression {params.WebGL_build}})
             steps {
                 echo "Build WebGL executada em: ${new Date()}"
                 bat """
                 "${env.UNITY_PATH}" -quit -batchmode -projectPath "${env.PROJECT_PATH}" -executeMethod BuildScript.BuildWebGL -logFile -
                 """
             }
-        }*/
+        }
     }
     
     post {
         always {
-            archiveArtifacts artifacts: '**/*.zip', allowEmptyArchive: true
-            archiveArtifacts artifacts: '**/TestResults/*.xml', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'Builds/**.zip', allowEmptyArchive: true
+            archiveArtifacts artifacts: 'Builds/TestResults/*.xml', allowEmptyArchive: true
         }
     }
 }
