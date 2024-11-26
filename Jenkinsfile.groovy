b0b74256fca1bdeaf8207d7bc462c1b41fee506b
Pedro Henrique Passos Carreira <peagacarreira@gmail.com>
2024-11-26 00:36:45 -0300

    sera ?

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -35,15 +35,6 @@ pipeline {
              }
         }
 
-        stage('Build WebGL') {
-            when({expression {params.WebGL_build}})
-            steps {
-                echo "Build WebGL executada em: ${new Date()}"
-                bat """
-                "${env.UNITY_PATH}" -quit -batchmode -projectPath "${env.PROJECT_PATH}" -executeMethod BuildScript.BuildWebGL -logFile -
-                """
-            }
-        }
     }
     
     post {
